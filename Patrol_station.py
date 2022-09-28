73f97f378d069548b45e4b8efd1771cc75e75324
Muhammad Bilal <mbilalcr07@gmail.com>
2022-09-28 17:01:48 +0500

    .

---
+++ Patrol_station.py
@@ -0,0 +1,14 @@
+'''
+Write a python program that finds out how much petrol (in liter) has been filled up in a car. 
+Customers will be asked how much petrol they want to fill up. 
+Given that 1L = 250.10 PKR.
+The Program should print a reciept, for example:
+You filled up 13.32 L of petrol of price = 150.1 today.
+'''
+price = 250.10
+
+to_fill=float(input("How much do you want to fill up : "))
+
+litters=round(to_fill/price,2)
+
+print("You filled up {0} L petrol of price = 250.10 today ".format(litters))
---
+++ bank.py
@@ -0,0 +1,20 @@
+'''Write a program that opens a bank account for users. The program should ask and store the following data of the user.
+1. Name
+2. Initial deposit amount
+
+At the end, the user withdraws an amount equal to 10% of the initial deposited amount, and the program will display a reciept containing:
+a. The name of the customer
+b. His remaining balance.
+c. Cash withdrawn  
+For example: Ali, you have withdrawn 200.0 PKR. Your remaining balance is: 1800.0'''
+
+
+
+name=input("Enter your name : ")
+balnace =float(input("Enter your initial deposite : "))
+
+withdraw=balnace   * 0.1 
+balnace =balnace - withdraw
+
+print("{0},you have withdraw {1} PKR .Your remaining balance is {2} PKR".format(name,withdraw,balnace) )
+
